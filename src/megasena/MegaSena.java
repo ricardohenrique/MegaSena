aa52b57064701ec4a5eb684a9dc97b263e4b82e4
ricardohenrique <ricardo.hb@outlook.com>
2016-10-28 20:13:47 -0200

    contagem de primeiras duplas funcionando

--- src/megasena/MegaSena.java
+++ src/megasena/MegaSena.java
@@ -18,15 +18,22 @@ public class MegaSena {
         int matriz[][] = new int[3000][quantidadeDeNumeroSorteado];
         int countNumeroX[][] = new int[61][2];
         int vetorOrdenado[] = new int[6];
+        int matrizCountDupla[][] = new int[61][61];
         Sort sort = new Sort();
         
         
         Random random = new Random();
-        String resultado = "", resultadoFinal = "", resultadoOrdenado = "", resultadoQuantidadePorNumero = "";
+        String resultado = "", resultadoFinal = "", resultadoOrdenado = "", resultadoQuantidadePorNumero = "", resultadoDupla = "";
         
         for (int i = 0; i < 61; i++) {
             countNumeroX[i][0] = i;
             countNumeroX[i][1] = 0;
+        }
+        
+        for (int i = 0; i < 61; i++) {
+            for (int j = 0; j < 61; j++) {
+                matrizCountDupla[i][j] = 0;
+            }
         }
         
         for (linha = 0; linha < 3000; linha++) {
@@ -41,7 +48,7 @@ public class MegaSena {
             }
         }
 
-        for (linha = 0; linha < 3000; linha++) {
+        for (linha = 0; linha < 3; linha++) {
             for(coluna = 0; coluna < quantidadeDeNumeroSorteado; coluna++){
                 resultado = resultado + String.format("%1$02d ", matriz[linha][coluna]);
                 vetorOrdenado[coluna] = matriz[linha][coluna];
@@ -50,16 +57,34 @@ public class MegaSena {
             for (coluna = 0; coluna < quantidadeDeNumeroSorteado; coluna++) {
                 resultadoOrdenado = resultadoOrdenado + String.format("%1$02d ", vetorOrdenado[coluna]);
             }
-            resultadoFinal = String.format("Sorteio[%1$04d]: " + resultado + "[ " + resultadoOrdenado + "]", (linha+1));
+            for (int i = 0; i < 61; i++) {
+                for (int j = 0; j < 61; j++) {
+                    if (vetorOrdenado[0] == i && vetorOrdenado[1] == j) {
+                        matrizCountDupla[i][j] = matrizCountDupla[i][j] + 1;
+                    }
+                }
+            }
+            resultadoFinal = String.format("Sorteio[%1$04d]: " + resultado + "[ " + resultadoOrdenado + "]", (linha+1));;
             System.out.println(resultadoFinal);
             resultado = "";
             resultadoOrdenado = "";
         }
         
-        for (int i = 1; i < 61; i++) {
-            resultadoQuantidadePorNumero = resultadoQuantidadePorNumero + String.format("Numero %1$02d = %2$04d vezes\n", i, countNumeroX[i][1]);
+//        for (int i = 1; i < 61; i++) {
+//            resultadoQuantidadePorNumero = resultadoQuantidadePorNumero + String.format("Numero %1$02d = %2$04d vezes\n", i, countNumeroX[i][1]);
+//        }
+//        
+//        System.out.println(resultadoQuantidadePorNumero);
+
+        
+        for (int i = 0; i < 61; i++) {
+            for (int j = 0; j < 61; j++) {
+                resultadoDupla += matrizCountDupla[i][j] + " ";
+            }
+            resultadoDupla += "\n";
         }
-        System.out.println(resultadoQuantidadePorNumero);
+        System.out.println(resultadoDupla);
+
         
     }
     
